113ea8c6ddaa8421c2e6eb80c88f0d7336e834df
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2021-12-10 08:54:17 +0300

    Что добавил
    Добавлено чуть-чуть JS, сделана анимация на service, исправлена проблема с видео, там теперь есть управление

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
-document.getElementById('btnPlay').addEventListener('click', playMove, true);
-
-var myVideo = document.getElementsByTagName('video')[0];
-function playMove(){
-    if (document.getElementById('btnPlay').style.opacity == 1 
-    || document.getElementById('btnPlay').style.opacity == "")
-    {
-        document.getElementById("btnPlay").style.opacity = "0";
-        document.getElementById("btnPlay").style.transition = "1s"
-        console.log('работает на воспроизведение')
+//Переменная для того, чтобы убрать постер при первой загрузке страницы
+const myVideo = document.getElementsByTagName('video')[0];
+//Убирает POSTER при первом нажатии на него
+document.getElementById('btnPlay').addEventListener('click', hiden, true);
+function hiden(){
+    document.getElementById("btnPlay").style.visibility = "hidden";
+    document.getElementById("btnPlay").style.opacity = "0";
+    document.getElementById("btnPlay").style.transition = "0.7s";
         myVideo.play();
-    }
-    else if (myVideo.style.opacity == 0)
-    {
-        document.getElementById("btnPlay").style.opacity = "1";
-        document.getElementById("btnPlay").style.transition = "1s"
-        console.log('работает на паузу')
-        myVideo.pause();
-    }
-
-
+}
+//Добавляет POSTER, когда видео на паузе
+document.getElementsByTagName('video')[0].addEventListener("pause", videoStop, true);
+function videoStop(){
+    document.getElementById("btnPlay").style.visibility = "visible";
+    document.getElementById("btnPlay").style.opacity = "1";
+    document.getElementById("btnPlay").style.transition = "0.7s";
+}
+//Запускает видео и скрывает POSTER
+document.getElementsByTagName('video')[0].addEventListener("play", videoStart, true);
+function videoStart(){
+    document.getElementById("btnPlay").style.visibility = "hidden";
+    document.getElementById("btnPlay").style.opacity = "0";
+    document.getElementById("btnPlay").style.transition = "0.7s";
+}
+//Запрещает появление POSTERA, когда перематываешь видео
+document.getElementsByTagName('video')[0].addEventListener("seeking", videoSeeking, true);
+function videoSeeking(){
+    document.getElementById("btnPlay").style.visibility = "hidden";
 }
